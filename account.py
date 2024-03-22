2aaf224b7c03ea208bd749b8a28306b55a3975be
alexpaduch <alexpad4338@gmail.com>
2024-03-21 22:42:01 -0500

    Revert "Merge branch 'Feature-DB' of https://github.com/chandler-cook/Lockbox into Feature-DB"
    This reverts commit d9358fa255d5b10178d4de215252b5f584c7c4d0, reversing
    changes made to 803753d70635ba691a48c00d409ec647ffd00472.

--- account.py
+++ account.py
@@ -40,21 +40,21 @@ class Account():
     # Define the method to handle the signup process
     def perform_signup(self, username, pw, cpw): 
         # Retrieve the username entered by the user
-        # username
+        new_username = username
         # Retrieve the password entered by the user
-        # pw
+        new_password = pw
         # Retrieve the confirmation password entered by the user
-        # cpw
+        confirm_password = cpw
         
-        # username is being used directly
+        self.username = new_username
         
         # The user must put a username and password
-        if username == '' or pw == '':
+        if new_username == '' or new_password == '':
             self.gui.show_message("error", "You must have a username and password")
             return
 
         # Check if the entered password and confirmation password match
-        if pw != cpw:
+        if new_password != confirm_password:
             # If they don't match, print an error message
             self.gui.show_message("error", "The passwords entered do not match. Please try again")
             return
@@ -63,11 +63,11 @@ class Account():
         try:
             # This function attempts to insert a new user record into the database
             # It will raise an sqlite3.IntegrityError if the username already exists
-            self.db.insert_user(username, pw)
+            self.db.insert_new_user(new_username, new_password)
             self.gui.show_message("success", "New user has been created") # If the insertion is successful, display a success message
             print("User signed up successfully!")
             
-            self.gui.menu_page(username) #Opens the main menu page
+            self.gui.menu_page() #Opens the main menu page
         # Handle the case where the username already exists in the database
         except sqlite3.IntegrityError:
             # Display an error message prompting the user to choose a different username
--- main.py
+++ main.py
@@ -12,21 +12,11 @@ root.geometry("500x300") # set the initial size of the window
 customtkinter.set_appearance_mode("dark")
 customtkinter.set_default_color_theme("dark-blue")
 
-
-gui_instance = GUI(root, None, None) # Creating an instance of the GUI class, passing the root window and None as account instance.
-
-db_instance = Database(gui_instance) # Database instance, passing GUI instance
-
+db_instance = Database()
+gui_instance = GUI(root, None, db_instance) # Creating an instance of the GUI class, passing the root window and None as account instance.
 account_instance = Account(root, gui_instance, db_instance) # Creating an instance of the Account class, passing the root window and just created gui instance
 
-# Update instances with correct references
-gui_instance.db = db_instance
-gui_instance.account = account_instance
-account_instance.gui = gui_instance
-db_instance.gui = gui_instance  # Ensure the database has a reference to the GUI
+account_instance.gui = gui_instance # Update account_instance with gui_instance
+gui_instance.account = account_instance # Update gui_instance with account_instance
 
-# You could call this function at the start of your program to ensure the database and tables are set up
-db_instance.create_tables()
-
-#start app
 root.mainloop() # start customtkinter app
